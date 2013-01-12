ef3f296867dc71aa48b6cad69c950da5cd98ef35
Carlos Arenas <carlos.arenas@gmail.com>
2013-01-12 17:48:36 -0500

    Final report by phrase now includes phrases with zero hits

--- generate_search_hit_reports.R
+++ generate_search_hit_reports.R
@@ -13,7 +13,7 @@ Docs.with.single.Phrase <- aggregate(list("Docs with single Phrase"=subSR$Phrase
 
 # build final by-phrase report
 SR.by.Phrase <- merge (Hits.by.Phrase, Docs.by.Phrase)
-SR.by.Phrase <- merge (SR.by.Phrase,   Docs.with.single.Phrase)
+SR.by.Phrase <- merge (SR.by.Phrase,   Docs.with.single.Phrase, all.x=TRUE)
 
 # get by-custodian counts
 Hits.by.Custodian <- aggregate(list(Hits=SR$Hit.Count), SR["Custodian"], sum)
@@ -23,7 +23,7 @@ Docs.by.Custodian <- aggregate(list(Docs=SR$Doc.Id),    SR["Custodian"], length)
 SR.by.Custodian <- merge (Hits.by.Custodian, Docs.by.Custodian)
 
 # export final reports
-write.csv(SR.by.Phrase,    file="Search Hit Report by Phrase.csv",    row.names=FALSE)
+write.csv(SR.by.Phrase,    file="Search Hit Report by Phrase.csv",    row.names=FALSE, na="0")
 write.csv(SR.by.Custodian, file="Search Hit Report by Custodian.csv", row.names=FALSE)
 
 # get distinct-document count
